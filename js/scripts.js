e42b55ba15d519a97ff73dfba6d4086c38f206b5
Adrian Camacho <adriancamacho18@gmail.com>
2021-06-04 13:32:21 -0700

    add initial js langauge

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  ("#code").submit(function(event) {
+    event.preventDefault();
+    const age = parseInt($("#age").val());
+    const disney = $("#disney").val();
+    const denzel = $("#denzel").val();
+    const shoes = $("#shoes").val();
+    const anime = $("#anime").val();
+    const tp = $("#tp").val();
+  })
+})
