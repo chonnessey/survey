ff239a5de6bbd01faced5504efaaccc7755ebae9
Adrian Camacho <adriancamacho18@gmail.com>
2021-06-04 13:56:52 -0700

    add js functionality to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  ("#code").submit(function(event) {
+  $("#code").submit(function(event) {
     event.preventDefault();
     const age = parseInt($("#age").val());
     const disney = $("#disney").val();
@@ -7,5 +7,22 @@ $(document).ready(function() {
     const shoes = $("#shoes").val();
     const anime = $("#anime").val();
     const tp = $("#tp").val();
+    let result;
+
+    if(age > 1 && disney === "wall-e" ) {
+      result = "#swift"
+    } else if(age > 10 && denzel === "training") {
+      result = "#js"
+    } else if(shoes === "crocs" && anime === "dbz") {
+      result = "#c"
+    } else if(age >= 21 && tp === "cottenelle") {
+      result = "#ruby"
+    }
+
+    $(".hide-me").hide();
+    $(result).show("#swift");
+    $(result).show("#js");
+    $(result).show("#c");
+    $(result).show("#ruby");
   })
 })
